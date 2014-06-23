f42df16781226cdbc1b7a1be4cec13071b330ca9
Alessio Santo <alessio.santocs@gmail.com>
2014-06-23 21:47:02 +0200

    Added google form and modified copy and navbar

--- website/js/main.js
+++ website/js/main.js
@@ -1,3 +1,15 @@
 require(["lib/jquery.min"], function (jquery) {
 	require(["lib/bootstrap.min"]);
+
+	// ACTUAL CODE
+	$("#CaptainEmailForm").submit(function (event) {
+		event.preventDefault();
+
+		var form = $(this);
+		var data = form.serialize();
+
+		$.post(form.attr("action"), data);
+
+		form.addClass("success");
+	})
 });
