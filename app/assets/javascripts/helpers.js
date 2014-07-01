0e7a720d45f4a857ad4b2aa3bf26cf157dcb0f67
Alessio Santo <alessio.santocs@gmail.com>
2014-07-01 23:18:07 +0200

    Improvements and bugfixing

--- app/assets/javascripts/helpers.js
+++ app/assets/javascripts/helpers.js
@@ -3,4 +3,9 @@ $(document).ready(function () {
 	$(".btn-action-refresh").click(function () {
 		window.location.reload();
 	});
+
+	$(".application_alerts").css("height", $(".application_alerts").innerHeight());
+	$(".application_alerts").click(function (argument) {
+		$(this).addClass("read");
+	});
 });
