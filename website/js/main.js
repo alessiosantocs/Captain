3085515be417a031457224e924a5438b9ddd8f60
Alessio Santo <alessio.santocs@gmail.com>
2014-07-25 10:43:48 +0200

    The what is done

--- website/js/main.js
+++ website/js/main.js
@@ -1,26 +1,32 @@
-require(["lib/jquery.min"], function (jquery) {
+
+requirejs.config({
+   "paths": {
+     "jquery": "lib/jquery.min",
+     "waypoints": "lib/waypoints.min"
+   }
+});
+
+require(["jquery", "waypoints"], function (jquery) {
 	require(["lib/bootstrap.min", "lib/vertical-align", "lib/window-sized", "lib/jquery.scrollTo.min", "lib/prettify"]);
 
 	$(document).ready(function () {
 		// ACTUAL CODE
 
-		$(document).ready(function () {
-			$('a').click(function(event){
-				event.preventDefault();
+		$('a').click(function(event){
+			event.preventDefault();
 
-				var href = $.attr(this, 'href');
-				$.scrollTo($(href), 1000, {
-					onAfter: function () {
-						// window.location.hash = href;
-					},
-					offset: -50
-				});
+			var href = $.attr(this, 'href');
+			$.scrollTo($(href), 1000, {
+				onAfter: function () {
+					// window.location.hash = href;
+				},
+				offset: -50
 			});
-
-			window.setTimeout(function () {
-				window.prettyPrint && prettyPrint()
-			}, 500)
 		});
+
+		window.setTimeout(function () {
+			window.prettyPrint && prettyPrint()
+		}, 500)
 
 		$("#CaptainEmailForm").submit(function (event) {
 			event.preventDefault();
@@ -32,8 +38,20 @@ require(["lib/jquery.min"], function (jquery) {
 
 			form.addClass("success");
 		});
+	
+		$(".waypointed").waypoint(function (direction) {
+			classname = $(this).attr("data-waypoint-class");
+			console.log(direction);
+			$(this).removeClass("justhide");
+			if(direction == "down")
+				$(this).addClass(classname);
+			else
+				$(this).removeClass(classname);
+		},{
+			triggerOnce: true,
+			offset: 250
+		});
 	});
-
 
 
 });
